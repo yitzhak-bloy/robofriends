86a9d2eeb97c1c3adf0c203a3210f0557bce907a
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-01-22 13:04:34 +0200

    add build

---
+++ build/precache-manifest.13146257e4147d2a263a3ed532e0482c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05ed99e7296eac688d6c",
+    "url": "/static/js/main.05ed99e7.chunk.js"
+  },
+  {
+    "revision": "686d2c9667bb73b2b9eb",
+    "url": "/static/js/1.686d2c96.chunk.js"
+  },
+  {
+    "revision": "05ed99e7296eac688d6c",
+    "url": "/static/css/main.beccd243.chunk.css"
+  },
+  {
+    "revision": "686d2c9667bb73b2b9eb",
+    "url": "/static/css/1.eb933057.chunk.css"
+  },
+  {
+    "revision": "05f185b43a4cddbeffdb4d1d5bfe37ca",
+    "url": "/index.html"
+  }
+];
